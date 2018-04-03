25989382fe36e1cf2579c8966d8392b8978dd9a5
Matt C <matthew.critelli@gmail.com>
2018-04-03 17:11:00 -0600

    Add rScale to change size of circle radius based on median age. Add fScale to show urban population rate on a color scale from green (low) to blue (high).

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Set SVG attributes as variables
 const height = 500;
 const width = 500;
-const padding = 35;
+const padding = 50;
 const scatterplot = d3.select("svg")
 
 // Filter data to remove null values for subscribers & literacy rate
-const filteredData = regionData.filter(d => {
-  return d.subscribersPer100 !== null && d.adultLiteracyRate !== null
-});
+
+
+const filteredData = regionData.filter(confirmKeys);
 
 // Set X and Y-axis scales
 const xScale = d3.scaleLinear()
@@ -18,12 +18,23 @@ const yScale = d3.scaleLinear()
                  .domain(d3.extent(filteredData, d => d.subscribersPer100))
                  .range([height - padding, padding]);
 
+const rScale = d3.scaleLinear()
+                .domain(d3.extent(filteredData, d => d.medianAge))
+                .range([5, 20]);
+
+const fScale = d3.scaleLinear()
+                     .domain(d3.extent(filteredData, d => d.urbanPopulationRate))
+                     .range(['green', 'blue'])
+
+
 // Add X & Y-Axis tick marks and format to remove overflow lines
 const xAxis = d3.axisBottom(xScale)
+                .tickPadding(15)
                 .tickSize(-height + padding * 2)
                 .tickSizeOuter(0);
 
 const yAxis = d3.axisLeft(yScale)
+                .tickPadding(10)
                 .tickSize(-width + padding * 2)
                 .tickSizeOuter(0);
 
@@ -49,14 +60,15 @@ scatterplot
   .append("circle")
     .attr("cx", d => xScale(d.adultLiteracyRate))
     .attr("cy", d => yScale(d.subscribersPer100))
-    .attr("r", 5)
+    .attr("r", d => rScale(d.medianAge))
+    .attr("fill", d => fScale(d.urbanPopulationRate))
 
 // Chart title
 scatterplot
   .append("text")
     .text("Cellular Subscriptions vs Literacy Rate")
     .attr("x", width / 2)
-    .attr("y", padding - 10)
+    .attr("y", padding / 2)
     .style("text-anchor", "middle")
     .style("font-size", "1.2em")
 
@@ -65,8 +77,7 @@ scatterplot
   .append("text")
     .text("Adult Literacy Rate (Over 15 yrs. old)")
     .attr("x", width / 2)
-    .attr("y", height - padding)
-    .attr("dy", "1.5em")
+    .attr("y", height - 10)
     .style("text-anchor", "middle")
 
 // Y-Axis Label
@@ -74,7 +85,17 @@ scatterplot
   .append("text")
     .text("Cellular Subscribers per 100 People")
     .attr("x", -height / 2)
-    .attr("y", padding)
-    .attr("dy", "-1.5em")
+    .attr("y", 15)
     .attr("transform", "rotate(-90)")
     .style("text-anchor", "middle")
+
+
+// Helper function to remove null values from data set
+function confirmKeys(obj) {
+  const reqValues = ['subscribersPer100', 'adultLiteracyRate','urbanPopulationRate', 'medianAge'];
+
+  for(let dataPoint of reqValues){
+    if (obj[dataPoint] === null) return false
+  }
+  return true
+}
